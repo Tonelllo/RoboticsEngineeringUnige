18ae31200bec3f488336133c5dae4ddfa4b2e9f7
Davide Tonelli <79359121+Tonelllo@users.noreply.github.com>
2023-10-13 23:48:29 +0200

    Update pdfcreator.sh

--- PdfScript/pdfcreator.sh
+++ PdfScript/pdfcreator.sh
@@ -1,17 +1,14 @@
 readarray -d '' dir_array < <(find . -type d -name "*-DT" -print0)
 
-$array
-
 for i in ${dir_array[@]}
 do
     echo "found DT directory "  $i
     readarray -d '' array < <(find $i -name "*.svgz" -print0)
-done
-
-for i in ${array[@]}
-do
-    echo "processing " $i
-    out=${i%.svgz}.pdf
-    out1=${out#./}
-    ./Write/Write --exit --out $out1 $i
+    for j in ${array[@]}
+    do
+        echo "processing " $j
+        out=${i%.svgz}.pdf
+        out1=${out#./}
+        ./Write/Write --exit --out $out1 $j
+    done
 done
